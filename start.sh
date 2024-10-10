a4171bc12c309d6834dd14c8cc2211be61753357
Diego Avelar <diego.avelar@om30tech.com.br>
2024-10-10 17:38:51 -0300

    subindo versão 1.0.0

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Create database file
+touch /var/www/html/database/database.sqlite
+
 # Set permissions for the SQLite database file
 chown -R root:root /var/www/html/database
 chmod -R 777 /var/www/html/database
